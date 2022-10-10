8a058801d46a9e7d4efc00e62fc50763091bf2c0
enetsos <jetw.ncc@gmail.com>
2022-10-10 13:38:33 +0200

    serie 1 es 2

--- src/serie1/es2/Main.java
+++ src/serie1/es2/Main.java
@@ -11,5 +11,15 @@ public class Main {
         lista1.printLista();
         lista1.rimuovi(3);
         lista1.printLista();
+
+        Lista<Double> lista2 = new Lista<>();
+
+        lista2.inserisciInCoda(4.2);
+        lista2.inserisciInCoda(3.5);
+        lista2.printLista();
+        lista2.sort();
+        lista2.printLista();
+        lista2.rimuovi(3.5);
+        lista2.printLista();
     }
 }
