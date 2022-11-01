0dc81915634b6c633f71b2b58740b29e0c399e6e
Sostene <jetw.ncc@gmail.com>
2022-11-01 15:16:43 +0100

    serie 4

--- src/serie4/es1/Main.java
+++ src/serie4/es1/Main.java
@@ -1,31 +1,56 @@
 package serie4.es1;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // FIXME implement tests to verify methods
-        Motorbike moto = new Motorbike("Honda", Motorbike.Type.STREET);
+    public static void main(String[] args) throws ClassNotFoundException {
+        String startClassName = "serie4.es1.NascarCar";
+        String endClassName = "serie4.es1.F1Car";
 
-        System.out.println();
+        System.out.println(getPathToObject(startClassName));
+        System.out.println(getPathToClass(startClassName, endClassName));
+        System.out.println(getCommonAncestor(startClassName, endClassName));
     }
 
+    private static List<String> getPathToObject(String startClassName) throws ClassNotFoundException {
+        List<String> list = new ArrayList<>();
+        Class<?> class_ = Class.forName(startClassName);
 
-    private static List<String> getPathToClass(String startClassName, String endClassName) {
-        // FIXME to implement
-        return Collections.emptyList();
+        while (class_ != null) {
+            list.add(class_.getSimpleName());
+
+            class_ = class_.getSuperclass();
+        }
+        return list;
     }
 
-    private static List<String> getPathToObject(String startClassName) {
-        // FIXME to implement
-        return Collections.emptyList();
+    private static List<String> getPathToClass(String startClassName, String endClassName) throws ClassNotFoundException {
+        List<String> list = new ArrayList<>();
+        Class<?> class_ = Class.forName(startClassName);
+        Class<?> endClass_ = Class.forName(endClassName);
+
+        while (!class_.getName().equals(endClass_.getName())) {
+            list.add(class_.getSimpleName());
+
+            class_ = class_.getSuperclass();
+
+            if (class_ == null)
+                return Collections.emptyList();
+        }
+        return list;
     }
 
+    private static String getCommonAncestor(String className0, String className1) throws ClassNotFoundException {
+        List<String> path0 = null;
+        List<String> path1 = null;
 
-    private static String getCommonAncestor(String className0, String className1) {
-        // FIXME to implement
-        return "To implement!";
+        path0 = getPathToObject(className0);
+        path1 = getPathToObject(className1);
+        path0.retainAll(path1);
+
+        return path0.get(0);
     }
 }
